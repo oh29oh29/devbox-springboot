5848039ee5c3ba949f96d86f25cf524a726cf95b
oh29oh29 <hyukjae89@gmail.com>
2025-02-24 21:05:42 +0900

    [NoSQL | Redis] code based configuration

--- nosql-redis/src/main/kotlin/com/oh29oh29/devbox/springboot/nosql/redis/config/RedisConfig.kt
+++ nosql-redis/src/main/kotlin/com/oh29oh29/devbox/springboot/nosql/redis/config/RedisConfig.kt
@@ -3,10 +3,20 @@ package com.oh29oh29.devbox.springboot.nosql.redis.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisPassword
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.StringRedisTemplate
 
 @Configuration
 class RedisConfig {
+
+    @Bean
+    fun lettuceConnectionFactory(): LettuceConnectionFactory = LettuceConnectionFactory(
+        RedisStandaloneConfiguration("localhost", 16379).apply {
+            this.password = RedisPassword.of("myredis")
+        },
+    )
 
     @Bean
     fun redisTemplate(connectionFactory: RedisConnectionFactory): StringRedisTemplate {
