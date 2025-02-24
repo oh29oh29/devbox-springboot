170660b7afa75ed07fcb73f845446bc362f07095
oh29oh29 <hyukjae89@gmail.com>
2025-02-24 21:06:57 +0900

    [NoSQL | Redis] zSetOperations(sortedSet)

--- nosql-redis/src/main/kotlin/com/oh29oh29/devbox/springboot/nosql/redis/service/RedisService.kt
+++ nosql-redis/src/main/kotlin/com/oh29oh29/devbox/springboot/nosql/redis/service/RedisService.kt
@@ -1,28 +1,48 @@
 package com.oh29oh29.devbox.springboot.nosql.redis.service
 
 import org.springframework.data.redis.core.StringRedisTemplate
-import org.springframework.data.redis.core.ValueOperations
+import org.springframework.data.redis.core.ZSetOperations
 import org.springframework.stereotype.Service
-import java.time.LocalDateTime
 
 @Service
 class RedisService(
-    redisTemplate: StringRedisTemplate
+    private val redisTemplate: StringRedisTemplate
 ) {
-    private final val ops: ValueOperations<String, String> = redisTemplate.opsForValue()
+    private final val zSetOperations: ZSetOperations<String, String> = redisTemplate.opsForZSet()
 
     init {
+        initialize()
         setValue()
         getValue()
     }
 
+    private fun initialize() {
+        redisTemplate.delete(listOf("main:20240101"))
+    }
+
     private fun setValue() {
-        ops.set("a", "a: ${LocalDateTime.now()}")
-        ops.set("b", "b: ${LocalDateTime.now()}")
+        zSetOperations.add("main:20240101", "sub:a", 100.0)
+        zSetOperations.add("main:20240101", "sub:b", 50.0)
+        zSetOperations.add("main:20240101", "sub:c", 200.0)
+        zSetOperations.add("main:20240101", "sub:d", 150.0)
+
+        zSetOperations.incrementScore("main:20240101", "sub:d", 1.0)
     }
 
     private fun getValue() {
-        println(ops.get("a"))
-        println(ops.get("b"))
+        println(zSetOperations.range("main:20240101", 0L, -1L))
+        println(zSetOperations.rangeWithScores("main:20240101", 0L, -1L))
+        println(zSetOperations.reverseRange("main:20240101", 0L, -1L))
+        println(zSetOperations.reverseRangeWithScores("main:20240101", 0L, -1L))
+        println()
+        println(zSetOperations.score("main:20240101", "sub:a")!!)
+        println(zSetOperations.score("main:20240101", "sub:b")!!)
+        println(zSetOperations.score("main:20240101", "sub:c")!!)
+        println(zSetOperations.score("main:20240101", "sub:d")!!)
+        println()
+        println(zSetOperations.rank("main:20240101", "sub:a")!!)
+        println(zSetOperations.rank("main:20240101", "sub:b")!!)
+        println(zSetOperations.rank("main:20240101", "sub:c")!!)
+        println(zSetOperations.rank("main:20240101", "sub:d")!!)
     }
 }
