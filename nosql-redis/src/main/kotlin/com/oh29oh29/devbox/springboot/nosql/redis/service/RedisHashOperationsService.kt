c7047c07ec9c227773f14ec302006f175fb8d882
oh29oh29 <hyukjae89@gmail.com>
2025-02-24 21:21:18 +0900

    [NoSQL | Redis] hashOperations

---
+++ nosql-redis/src/main/kotlin/com/oh29oh29/devbox/springboot/nosql/redis/service/RedisHashOperationsService.kt
@@ -0,0 +1,42 @@
+package com.oh29oh29.devbox.springboot.nosql.redis.service
+
+import org.springframework.data.redis.core.HashOperations
+import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.stereotype.Service
+import java.time.LocalDateTime
+
+@Service
+class RedisHashOperationsService(
+    private val redisTemplate: StringRedisTemplate
+) {
+    private final val hashOperations: HashOperations<String, String, String> = redisTemplate.opsForHash()
+
+    init {
+        initialize()
+        setValue()
+        getValue()
+    }
+
+    private fun initialize() {
+        redisTemplate.delete(listOf("c", "d"))
+    }
+
+    private fun setValue() {
+        hashOperations.put("c", "c-hk", "[ c-hk ] ${LocalDateTime.now()}")
+        hashOperations.put("d", "d-hk", "[ d-hk ] ${LocalDateTime.now()}")
+
+        hashOperations.put("c", "c-hk", "[ c-hk-dup ] ${LocalDateTime.now()}")
+        hashOperations.put("d", "d-hk", "[ d-hk-dup ] ${LocalDateTime.now()}")
+
+        hashOperations.putIfAbsent("c", "c-hk", "[ c-hk-new ] ${LocalDateTime.now()}")
+        hashOperations.putIfAbsent("d", "d-hk", "[ d-hk-new ] ${LocalDateTime.now()}")
+
+        hashOperations.putIfAbsent("c", "c-hk-2", "[ e-hk-2 ] ${LocalDateTime.now()}")
+        hashOperations.putIfAbsent("d", "f-hk-2", "[ f-hk-2 ] ${LocalDateTime.now()}")
+    }
+
+    private fun getValue() {
+        println(hashOperations.get("c", "c-hk"))
+        println(hashOperations.get("d", "d-hk"))
+    }
+}
