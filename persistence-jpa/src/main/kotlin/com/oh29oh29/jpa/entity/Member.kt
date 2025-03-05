7de0a30b1fba936aeed72329ebd512764666cd4c
oh29oh29 <hyukjae89@gmail.com>
2025-03-05 14:48:01 +0900

    [Persistence | JPA] Indexes
    reference: https://www.baeldung.com/jpa-indexes

--- persistence-jpa/src/main/kotlin/com/oh29oh29/jpa/entity/Member.kt
+++ persistence-jpa/src/main/kotlin/com/oh29oh29/jpa/entity/Member.kt
@@ -2,11 +2,14 @@ package com.oh29oh29.jpa.entity
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
-import java.util.UUID
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import java.util.*
 
 @Entity
+@Table(indexes = [Index(name = "IDX_MEMBER_name_address", columnList = "name, address")])
 data class Member(
-    val name:  String,
+    val name: String,
     val address: String,
     val phoneNumber: String,
     val email: String,
--- persistence-jpa/src/main/kotlin/com/oh29oh29/jpa/service/PaymentReceiptService.kt
+++ persistence-jpa/src/main/kotlin/com/oh29oh29/jpa/service/PaymentReceiptService.kt
@@ -1,41 +1,13 @@
 package com.oh29oh29.jpa.service
 
-import com.oh29oh29.jpa.entity.Member
-import com.oh29oh29.jpa.entity.Payment
-import com.oh29oh29.jpa.entity.PaymentReceipt
 import com.oh29oh29.jpa.repository.MemberRepository
 import com.oh29oh29.jpa.repository.PaymentReceiptRepository
 import com.oh29oh29.jpa.repository.PaymentRepository
-import org.hibernate.proxy.HibernateProxy
 import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
 
 @Service
 class PaymentReceiptService(
     private val paymentReceiptRepository: PaymentReceiptRepository,
     private val paymentRepository: PaymentRepository,
     private val memberRepository: MemberRepository,
-) {
-    lateinit var paymentReceiptId: String
-
-    init {
-        init()
-    }
-
-    private fun init() {
-        val member = Member("user name", "address", "01012341234", "dev@dev.com")
-        memberRepository.save(member)
-
-        val payment = Payment(member)
-        paymentRepository.save(payment)
-
-        val paymentReceipt = PaymentReceipt(payment)
-        paymentReceiptId = paymentReceiptRepository.save(paymentReceipt).id
-    }
-
-    @Transactional
-    fun getPaymentReceipt() {
-        val paymentReceipt = paymentReceiptRepository.getReferenceById(paymentReceiptId)
-        println(paymentReceipt.payment is HibernateProxy)
-    }
-}
+)
